fc83bf2b5543d998542d9ac582a03b7c86555f4a
dipoengoro <dipoengoro@outlook.com>
2022-03-21 16:01:09 +0700

    Optimize build.gradle

--- app/src/main/java/id/dipoengoro/contact/fragment/AddEditFragment.kt
+++ app/src/main/java/id/dipoengoro/contact/fragment/AddEditFragment.kt
@@ -1,12 +1,11 @@
 package id.dipoengoro.contact.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import id.dipoengoro.contact.R
 import id.dipoengoro.contact.databinding.FragmentAddEditBinding
 
 class AddEditFragment : Fragment() {
